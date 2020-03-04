3e57b34306881997625a4228bb02adb87c52aa1a
kaelsteinert <kaelsteinert@gmail.com>
2020-03-04 15:57:01 -0500

    cart fragment update function added

--- wc-cart-dropdown.php
+++ wc-cart-dropdown.php
@@ -84,7 +84,7 @@ class cc_wc_cart_drop_widget extends WP_Widget {
 									<?php echo WC()->cart->get_cart_total(); ?> - <?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?>
 							</a>
 							<div class="cart-dropdown">
-								<div class="widget_shopping_cart_content"><?php woocommerce_mini_cart();?></div>
+								<div class="widget_shopping_cart_content"></div>
 								<?php if($my_account_location === 'inside') : ?>
 									<div class="my-account-inside">
 											<p class="woocommerce-mini-cart__buttons buttons">
@@ -161,3 +161,30 @@ class cc_wc_cart_drop_widget extends WP_Widget {
 		return $instance;
 	}
 } // Class cc_wc_cart_drop_widget ends here
+
+/**
+ * Ensure cart contents update when products are added to the cart via AJAX (place the following in functions.php).
+ * Used in conjunction with https://gist.github.com/DanielSantoro/1d0dc206e242239624eb71b2636ab148
+ * Compatible with 3.0.1+, for lower versions, remove "woocommerce_" from the first mention on Line 4
+ */
+function wc_header_add_to_cart_fragment_filter() {
+	if ( class_exists( 'woocommerce' ) ) {
+		add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+	}
+}
+add_action('after_setup_theme', 'wc_header_add_to_cart_fragment_filter');
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<a class="cart-customlocation" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo $woocommerce->cart->get_cart_total(); ?> - <?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?></a>
+	<?php
+
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+
+	return $fragments;
+
+}
